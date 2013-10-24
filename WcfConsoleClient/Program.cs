5ece9f2a72e20434ffc45953968608548a59f118
Gustav Dahlman <gustav.dahlman@outlook.com>
2013-10-24 18:51:19 +0200

    initial commit

---
+++ WcfConsoleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfConsoleClient
+{
+    class Program
+    {
+        private static  WcfServiceHost.Service1Client client = null;
+
+        static void Main(string[] args)
+        {
+            client = new WcfServiceHost.Service1Client();
+            CallService();    
+        }
+
+        private static void CallService()
+        {
+            ConsoleKeyInfo cki;
+            while (true)
+            {
+                Console.WriteLine("Press any key to call service, Press ESC to end.");
+                cki = Console.ReadKey();
+                if (cki.Key == ConsoleKey.Escape)
+                    break;
+
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine(client.GetData(cki.KeyChar.ToString()).ToString());
+            }
+        }
+    }
+}
---
+++ WcfServiceConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+namespace FirstWcfServiceHostConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (ServiceHost host = new ServiceHost(typeof(WcfServiceLibrary1.Service1)))
+            {
+                host.Open();
+                Console.WriteLine("Press <Enter> to terminate the Host application.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
